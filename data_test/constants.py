2465edd34a0c527ef4d2d672c28be10508fca6d4
Oleg <kov2905@icloud.com>
2024-02-25 21:43:55 +0200

    tests for update user name

--- data_test/constants.py
+++
@@ -1,6 +0,0 @@
-class Constants:
-    URL = 'https://stellarburgers.nomoreparties.site'
-    END_POINT_LOGIN_USER = '/api/auth/login'
-    END_POINT_CREAT_USER = '/api/auth/register'
-    END_POINT_UPDATE_USER = '/api/auth/user'
-    headers = {"Content-Type": "application/json"}
